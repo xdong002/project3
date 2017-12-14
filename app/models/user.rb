78e335974c7657ae9fef2aea9b171e4584f0b42a
unknown <xxx419168445@gmail.com>
2017-12-14 09:49:42 -0800

    fix some render bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: {minimum: 3}
+  validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 15}
   has_secure_password
   has_many :topics
   has_many :comments
